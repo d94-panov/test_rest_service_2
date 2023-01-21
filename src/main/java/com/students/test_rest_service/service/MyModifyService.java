1de9fc618168e5f8629353ff232d84d0100b9f1e
Dmitriy <d94-panov@mail.ru>
2023-01-21 17:34:13 +0500

    Initial commit

---
+++ src/main/java/com/students/test_rest_service/service/MyModifyService.java
@@ -0,0 +1,7 @@
+package com.students.test_rest_service.service;
+
+import com.students.test_rest_service.model.Response;
+
+public interface MyModifyService {
+    Response modify(Response response);
+}
